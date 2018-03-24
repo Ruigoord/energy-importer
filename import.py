b9f43b13d0db076516fa432856ec1efa2fe22d5a
Remco Wendt <remco.wendt@gmail.com>
2018-03-24 14:40:20 +0100

    Improved line reading and added progress bar

--- import.py
+++ import.py
@@ -6,6 +6,7 @@ import decimal
 import itertools
 
 import requests
+from progress.bar import IncrementalBar
 
 BUFFER_SIZE = 5000
 URL = 'http://localhost:8086/write?db=ruigoord'
@@ -20,11 +21,21 @@ def sanitize_number(s):
         return s
 
 
-def line_reader():
-    for filename in glob.iglob('**/*.CSV', recursive=True):
-        location = filename.split('/')[0]
+def get_total_lines(paths):
+    total_lines = 0
+    for path in paths:
+        with open(path) as f:
+            total_lines += sum(1 for l in f)
 
-        with open(filename) as csvfile:
+    return total_lines
+
+
+def line_reader(paths):
+    for path in paths:
+        # Location of sensor is first part of path
+        location = path.split('/')[0]
+
+        with open(path) as csvfile:
             csv_reader = csv.DictReader(csvfile, delimiter=';')
 
             for row in csv_reader:
@@ -34,7 +45,7 @@ def line_reader():
                     )
                 except ValueError:
                     # On invalid time values skip to next row
-                    print('Ivalid time:', row['Date'], row['Time'])
+                    # print('Ivalid time:', row['Date'], row['Time'])
                     continue
 
                 timestamp = int(t.timestamp())
@@ -49,23 +60,37 @@ def line_reader():
                     )
                 except AttributeError:
                     # On invalid value skip to next row
-                    print('Invalid measurement:', row)
+                    # print('Invalid measurement:', row)
                     continue
 
                 yield f'{location} value={value} {timestamp}'
 
 
-if __name__ == "__main__":
-    line_reader = line_reader()
-
+def buffered_line_reader(lines, buffer_size):
     try:
-        with requests.Session() as session:
-            while True:
-                data = '\n'.join(itertools.islice(line_reader, BUFFER_SIZE))
-                response = session.post(URL, data=data)
-                assert response.status_code == 204, response.text
+        while True:
+            yield '\n'.join(itertools.islice(lines, buffer_size))
     except StopIteration:
         pass
 
 
+if __name__ == "__main__":
+    # We can't use iglob since we need the glob twice
+    all_csv_files = glob.glob('**/*.CSV', recursive=True)
 
+    total_size = get_total_lines(all_csv_files) / BUFFER_SIZE
+
+    bar = IncrementalBar(
+        # max=total_size, suffix='%(percent)d%% [%(index)d/%(max)d]'
+        max=total_size, suffix='%(percent)d%% [ETA: %(eta_td)s]'
+    )
+    bar.start()
+
+    line_reader = buffered_line_reader(
+        line_reader(all_csv_files), buffer_size=BUFFER_SIZE
+    )
+
+    with requests.Session() as session:
+        for lines in bar.iter(line_reader):
+            response = session.post(URL, data=lines)
+            assert response.status_code == 204, response.text
